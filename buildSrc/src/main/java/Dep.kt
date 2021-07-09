e23adc49667426df121a61ca3d88bd0f7a4225dc
pluulove <pluulove@gmail.com>
2021-07-09 21:13:18 +0900

    First Commit

---
+++ buildSrc/src/main/java/Dep.kt
@@ -0,0 +1,20 @@
+object Dep {
+    object AndroidX {
+
+    }
+    object Compose {
+
+    }
+
+    object Kotlin {
+
+    }
+
+    object Dagger {
+
+    }
+
+    object Accompanist {
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // Warning: this repository is going to shut down soon
+    }
+}
+rootProject.name = "DroidKnights2021"
+include(":app")
+include(":data")
+include(":domain")
+include(":features:ui-core")
+include(":features:ui-core-compose")
+include(":features:home")
+include(":features:schedule")
+include(":features:setting")
+include(":features:detail")
