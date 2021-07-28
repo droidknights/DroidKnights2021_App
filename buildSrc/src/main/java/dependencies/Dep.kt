e2c821afbd5295aa7541971ba71c489056c5edc0
pluulove <pluulove@gmail.com>
2021-07-29 00:40:02 +0900

    설정 화면에 Compose 환경 적용

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -29,9 +29,10 @@ object Dep {
         const val activityCompose = "androidx.activity:activity-compose:1.3.0-rc01"
 
         object Navigation {
-            private const val version = "2.3.5"
+            private const val version = "2.4.0-alpha05"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
+            const val composoe = "androidx.navigation:navigation-compose:$version"
         }
     }
 
