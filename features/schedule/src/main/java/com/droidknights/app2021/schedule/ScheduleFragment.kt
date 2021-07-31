c3b46cd19e4d7b56dade3682d0316594f75f5687
pluulove <pluulove@gmail.com>
2021-07-31 12:42:00 +0900

    에러 노출 중복 코드 개선

--- features/schedule/src/main/java/com/droidknights/app2021/schedule/ScheduleFragment.kt
+++ features/schedule/src/main/java/com/droidknights/app2021/schedule/ScheduleFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -21,7 +24,6 @@ import com.droidknights.app2021.core.ui.compose.setThemeContent
 import com.droidknights.app2021.core.ui.compose.state.UiState
 import com.droidknights.app2021.navigator.DetailNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -38,6 +40,8 @@ class ScheduleFragment : Fragment() {
     ): View {
         return setThemeContent {
             val result by viewModel.sessions.observeAsState(UiState.loading())
+            val scrollState = rememberScrollState()
+
             LoadingContent(
                 loading = result.initialLoad,
                 loadingContent = { FullScreenLoading() },
@@ -46,18 +50,21 @@ class ScheduleFragment : Fragment() {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
+                            .verticalScroll(scrollState)
                             .padding(24.dp),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Icon(
-                            painter = painterResource(id = R.drawable.ic_error_black_24),
+                            imageVector = Icons.Filled.Warning,
                             modifier = Modifier.size(64.dp),
                             contentDescription = null
                         )
                         Spacer(modifier = Modifier.height(10.dp))
-                        Text(text = result.exception?.localizedMessage.orEmpty())
-                        Timber.e(result.exception)
+                        Text(
+                            text = result.exception?.stackTraceToString().orEmpty(),
+                            modifier = Modifier.fillMaxWidth()
+                        )
                     }
                 }
             ) {
