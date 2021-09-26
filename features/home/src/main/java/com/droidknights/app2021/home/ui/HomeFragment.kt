bbc2abf9c91a3bc2153abf8046e7699c52d4806a
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-09-26 17:03:24 +0900

    [feature/#2] 코드리뷰 반영
    - LifeCycleObserver 활용해서 뷰가 파괴되지 않는 ON_STOP에서도 스크롤이 중단될 수 있게 설정

--- features/home/src/main/java/com/droidknights/app2021/home/ui/HomeFragment.kt
+++ features/home/src/main/java/com/droidknights/app2021/home/ui/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ConcatAdapter
 import com.droidknights.app2021.home.R
 import com.droidknights.app2021.home.databinding.FragmentHomeBinding
@@ -15,8 +14,6 @@ import com.droidknights.app2021.home.ui.adapter.EventAdapter
 import com.droidknights.app2021.home.ui.adapter.HeaderAdapter
 import com.droidknights.app2021.home.ui.adapter.InfoAdapter
 import com.droidknights.app2021.home.util.startOpenUrl
-import com.droidknights.app2021.shared.model.Event
-import com.droidknights.app2021.shared.model.Sponsor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,14 +21,17 @@ class HomeFragment : Fragment() {
 
     private val viewModel by viewModels<HomeViewModel>()
 
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "Wrong State" }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        _binding?.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
@@ -41,7 +41,7 @@ class HomeFragment : Fragment() {
             val concatAdapter = ConcatAdapter(
                 ConcatAdapter.Config.Builder().setIsolateViewTypes(false).build(),
                 HeaderAdapter(),
-                InfoAdapter(lifecycleScope, it.sponsors) { sponsor ->
+                InfoAdapter(it.sponsors) { sponsor ->
                     requireContext().startOpenUrl(sponsor.homepage)
                 },
                 EventAdapter(it.events) { event -> requireContext().startOpenUrl(event.url) }
@@ -49,5 +49,10 @@ class HomeFragment : Fragment() {
             binding.recyclerView.adapter = concatAdapter
             binding.recyclerView.addItemDecoration(EventItemDecoration(view.context))
         }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- features/home/src/main/java/com/droidknights/app2021/home/ui/adapter/InfoAdapter.kt
+++ features/home/src/main/java/com/droidknights/app2021/home/ui/adapter/InfoAdapter.kt
@@ -2,6 +2,7 @@ package com.droidknights.app2021.home.ui.adapter
 
 import android.annotation.SuppressLint
 import android.view.MotionEvent
+import androidx.lifecycle.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.droidknights.app2021.home.R
@@ -12,11 +13,11 @@ import com.droidknights.app2021.home.util.recyclerview.ItemDiffCallback
 import com.droidknights.app2021.home.util.recyclerview.ListBindingAdapter
 import com.droidknights.app2021.shared.model.Sponsor
 import kotlinx.coroutines.*
+import timber.log.Timber
 
 private const val SCROLL_DX = 25
 
 internal class InfoAdapter(
-    private val coroutineScope: CoroutineScope,
     sponsors: List<Sponsor>,
     private val itemHandler: ItemHandler
 ) : ListBindingAdapter<InfoItem>(ItemDiffCallback(
@@ -30,17 +31,80 @@ internal class InfoAdapter(
         }
     }
 
-    private var scrollJob: Job? = null
-
     init {
         submitList(listOf(InfoItem(sponsors)))
+    }
+
+    class SponsorViewLifeCycleObserver(
+        private val holder: DataBindingViewHolder<InfoItem>
+    ) : LifecycleObserver {
+        private var scrollJob: Job? = null
+
+        @SuppressLint("ClickableViewAccessibility")
+        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        fun startSponsorScroll() {
+            scrollJob = holder.itemView.findViewTreeLifecycleOwner()?.lifecycleScope?.launch {
+                (holder.binding as ItemInfoHeaderBinding).sponsorList.launchAutoScroll(
+                    holder.itemView.findViewTreeLifecycleOwner()?.lifecycleScope
+                )
+            }
+
+            (holder.binding as ItemInfoHeaderBinding).sponsorList.setOnTouchListener { v, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        scrollJob?.cancel()
+                        true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        scrollJob =
+                            holder.itemView.findViewTreeLifecycleOwner()?.lifecycleScope?.launch {
+                                holder.binding.sponsorList.launchAutoScroll(
+                                    holder.itemView.findViewTreeLifecycleOwner()?.lifecycleScope
+                                )
+                            }
+                        true
+                    }
+                    else -> true
+                }
+            }
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun stopScroll() {
+            (holder.binding as ItemInfoHeaderBinding).sponsorList.setOnTouchListener(null)
+            scrollJob?.cancel()
+        }
+
+        private tailrec suspend fun RecyclerView.launchAutoScroll(lifeCycleScope: LifecycleCoroutineScope?) {
+            Timber.d("Call launchAutoScroll")
+            val firstVisibleItem =
+                (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+            if (firstVisibleItem != RecyclerView.NO_POSITION && firstVisibleItem != 0) {
+                lifeCycleScope?.launch {
+                    val defferedAutoScroll = async {
+                        val currentList = (adapter as SponsorAdapter).currentList.toMutableList()
+                        val secondPart = currentList.subList(0, REPLACE_POSITION)
+                        val firstPart = currentList.subList(REPLACE_POSITION, currentList.size)
+                        val newList = mutableListOf<Sponsor>().apply {
+                            addAll(firstPart)
+                            addAll(secondPart)
+                        }
+                        (adapter as SponsorAdapter).submitList(newList)
+                    }
+                    defferedAutoScroll.await()
+                }
+            }
+            delay(25L)
+            smoothScrollBy(SCROLL_DX, 0)
+            launchAutoScroll(lifeCycleScope)
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
         return R.layout.item_info_header
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun viewBindViewHolder(holder: DataBindingViewHolder<InfoItem>, position: Int) {
         super.viewBindViewHolder(holder, position)
 
@@ -48,53 +112,19 @@ internal class InfoAdapter(
             sponsorList.adapter = SponsorAdapter(getItem(0).sponsors, sponsorItemHandler)
             sponsorList.addItemDecoration(SponsorItemDecoration())
         }
+    }
 
-        scrollJob = coroutineScope.launch {
-            holder.binding.sponsorList.launchAutoScroll(coroutineScope)
-        }
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewAttachedToWindow(holder: DataBindingViewHolder<InfoItem>) {
+        super.onViewAttachedToWindow(holder)
 
-        holder.binding.sponsorList.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    scrollJob?.cancel()
-                    true
-                }
-                MotionEvent.ACTION_UP -> {
-                    scrollJob = coroutineScope.launch {
-                        holder.binding.sponsorList.launchAutoScroll(coroutineScope)
-                    }
-                    true
-                }
-                else -> true
-            }
-        }
+        holder.itemView.findViewTreeLifecycleOwner()
+            ?.lifecycle
+            ?.addObserver(SponsorViewLifeCycleObserver(holder))
     }
 
     fun interface ItemHandler {
         fun clickSponsor(sponsor: Sponsor)
-    }
-
-    private tailrec suspend fun RecyclerView.launchAutoScroll(coroutineScope: CoroutineScope) {
-        val firstVisibleItem =
-            (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-        if (firstVisibleItem != RecyclerView.NO_POSITION && firstVisibleItem != 0) {
-            coroutineScope.launch {
-                val defferedAutoScroll = async {
-                    val currentList = (adapter as SponsorAdapter).currentList.toMutableList()
-                    val secondPart = currentList.subList(0, REPLACE_POSITION)
-                    val firstPart = currentList.subList(REPLACE_POSITION, currentList.size)
-                    val newList = mutableListOf<Sponsor>().apply {
-                        addAll(firstPart)
-                        addAll(secondPart)
-                    }
-                    (adapter as SponsorAdapter).submitList(newList)
-                }
-                defferedAutoScroll.await()
-            }
-        }
-        delay(25L)
-        smoothScrollBy(SCROLL_DX, 0)
-        launchAutoScroll(coroutineScope)
     }
 
     companion object {
