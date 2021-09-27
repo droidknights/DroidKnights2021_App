a04f05b436b4044a819cdb3b85e03a0092f00e18
Juyeop <kjy031104@gmail.com>
2021-09-27 16:10:05 +0900

    rename: 변수명 카멜케이스로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(Dep.AndroidX.core)
     implementation(Dep.AndroidX.appcompat)
     implementation(Dep.AndroidX.material)
-    implementation(Dep.AndroidX.constraintlayout)
+    implementation(Dep.AndroidX.constraintLayout)
     implementation(Dep.AndroidX.Navigation.fragment)
     implementation(Dep.AndroidX.Navigation.ui)
 
